d1394d44a4dafac188a957198f527ff1dec9e6c7
Siddharth Banga <91724657+VOSID8@users.noreply.github.com>
2023-01-12 02:16:02 +0530

    update

---
+++ learningspark/src/main/scala/standdardscala/trying.scala
@@ -0,0 +1,9 @@
+import scalafx.Includes._
+import scalafx.application.JFXApp
+import scalafx.collections.ObservableBuffer
+import scalafx.scene.Scene
+import scalafx.scene.chart._
+import scalafx.scene.chart.XYChart.Data
+import scalafx.scene.chart.Axis
+
+
